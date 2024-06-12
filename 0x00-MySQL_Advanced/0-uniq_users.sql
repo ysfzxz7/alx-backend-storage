6b7c1e8b708441b0bf84848466e5757e333eb19c
ysfzxz <bbbmaroc@gmail.com>
2024-06-12 16:25:19 +0100

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,8 @@
--- Create a table called users
--- Add id name and email to it
+-- Create a table of users
+-- Has 3 rows ID, EMAI and NAME
 
-CREATE TABLE IF NOT EISTS users (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+CREATE TABLE IF NOT EXISTS users (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
 );
-
