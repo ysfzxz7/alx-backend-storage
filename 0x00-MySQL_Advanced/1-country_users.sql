61c2fd50eae3ea8b3ae11e527e5ce415d98d2445
ysfzxz <bbbmaroc@gmail.com>
2024-06-12 16:35:11 +0100

    task1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,9 +2,9 @@
 -- Add id name email and country,
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)?
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)?
+  country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
 
